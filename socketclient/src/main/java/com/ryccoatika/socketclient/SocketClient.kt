ca25968b08f2edae4d4939ee0bfc39fe26a4278d
Rycco Atika <ryccoatika@gmail.com>
2023-10-16 07:40:44 +0700

    feat: client minimal feature

---
+++ socketclient/src/main/java/com/ryccoatika/socketclient/SocketClient.kt
@@ -0,0 +1,41 @@
+package com.ryccoatika.socketclient
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.DataOutputStream
+import java.net.Socket
+
+class SocketClient(
+    host: String,
+    port: Int,
+) {
+    private var socket: Socket? = null
+    private var outputStream: DataOutputStream? = null
+
+    init {
+        CoroutineScope(Dispatchers.IO).launch {
+            withContext(Dispatchers.IO) {
+                socket = Socket(host, port)
+                outputStream = DataOutputStream(socket?.getOutputStream())
+            }
+        }
+    }
+
+    fun disconnect() {
+        CoroutineScope(Dispatchers.IO).launch {
+            withContext(Dispatchers.IO) {
+                socket?.close()
+            }
+        }
+    }
+
+    fun sendMessage(message: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            withContext(Dispatchers.IO) {
+                outputStream?.writeUTF(message)
+            }
+        }
+    }
+}
